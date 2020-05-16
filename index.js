8fc37cd858d8857315a627551195937dd927d3cc
delins300 <62405023+delins300@users.noreply.github.com>
2020-05-16 12:54:10 -0300

    adicionei arquivos via upload

---
+++ index.js
@@ -0,0 +1,77 @@
+var titulo = document.querySelector(".titulo-principal");
+titulo.textContent = "Pimenta Nutrição";
+
+var paciente = document.querySelector("#primeiro-paciente");
+console.log(paciente);
+
+var tdPeso = paciente.querySelector(".info-peso");
+var peso = tdPeso.textContent;
+
+var tdAltura = paciente.querySelector(".info-altura");
+var altura = tdAltura.textContent;
+
+var imc = peso / (altura * altura);
+console.log(imc);
+
+var tdImc = paciente.querySelector(".info-imc");
+tdImc.textContent = imc;
+
+var paciente = document.querySelector("#segundo-paciente");
+console.log(paciente);
+
+var tdPeso = paciente.querySelector(".info-peso");
+var peso = tdPeso.textContent;
+
+var tdAltura = paciente.querySelector(".info-altura");
+var altura = tdAltura.textContent;
+
+var imc = peso / (altura * altura);
+console.log(imc);
+
+var tdImc = paciente.querySelector(".info-imc");
+tdImc.textContent = imc;
+
+var paciente = document.querySelector("#terceiro-paciente");
+console.log(paciente);
+
+var tdPeso = paciente.querySelector(".info-peso");
+var peso = tdPeso.textContent;
+
+var tdAltura = paciente.querySelector(".info-altura");
+var altura = tdAltura.textContent;
+
+var imc = peso / (altura * altura);
+console.log(imc);
+
+var tdImc = paciente.querySelector(".info-imc");
+tdImc.textContent = imc;
+
+var paciente = document.querySelector("#quarto-paciente");
+console.log(paciente);
+
+var tdPeso = paciente.querySelector(".info-peso");
+var peso = tdPeso.textContent;
+
+var tdAltura = paciente.querySelector(".info-altura");
+var altura = tdAltura.textContent;
+
+var imc = peso / (altura * altura);
+console.log(imc);
+
+var tdImc = paciente.querySelector(".info-imc");
+tdImc.textContent = imc;
+
+var paciente = document.querySelector("#quinto-paciente");
+console.log(paciente);
+
+var tdPeso = paciente.querySelector(".info-peso");
+var peso = tdPeso.textContent;
+
+var tdAltura = paciente.querySelector(".info-altura");
+var altura = tdAltura.textContent;
+
+var imc = peso / (altura * altura);
+console.log(imc);
+
+var tdImc = paciente.querySelector(".info-imc");
+tdImc.textContent = imc;
